b3b9e1e6bd8d5dceacb16a7612b74be40c6634c6
Felix Orinda <forinda82@gmail.com>
2022-07-02 12:47:09 +0300

    Update Navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faBell } from '@fortawesome/free-regular-svg-icons'
 import { Link } from 'react-router-dom';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import NavDropDownLinks from './NavDropDownLinks';
 const Header = () => {
 	return (
 		<div className='h-16 shadow-xl flex items-center justify-between px-4 sticky top-0 bg-white z-[1020]'>
-			<div>
+			<div className='flex gap-2'>
 				<Link to={'/'}>
 					<div className='flex items-center gap-1'>
 						<img
 							src='/logo512.png'
 							alt=''
-							className='w-10 h-10 object-cover rounded-full '
+							className='w-8 h-8 md:w-10 md:h-10 object-cover rounded-full '
 						/>
 						{/* <p className='uppercase font-bold hidden md:block'>Connect</p> */}
 					</div>
 				</Link>
+				<div className='flex items-center md:hidden group relative px-4'>
+					<FontAwesomeIcon icon={faBars} className="peer" size='lg'/>
+					<NavDropDownLinks/>
+				</div>
 			</div>
 			<div className='grow-1'>
 				<div className='flex items-center border px-2 py-2 rounded-[20px] '>
