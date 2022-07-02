7e4bdd4c481cab601b3403f5b0967bfb4fd9c907
Felix Orinda <forinda82@gmail.com>
2022-07-02 12:28:01 +0300

    Update env variables

---
+++ src/env/index.tsx
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
+const config = {
+	API_KEY: process.env.REACT_APP_BASE_API_KEY || '',
+	BACKEND_BASE_URL: process.env.REACT_APP_BACKEND_BASE_URL || '',
+	ENV: process.env.REACT_APP_NODE_ENV || '',
+};
+
+export default config;
+export { config };
