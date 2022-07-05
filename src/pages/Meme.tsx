f95da6882dd266a0f552162361aa1838df72c16a
Felix Orinda <forinda82@gmail.com>
2022-07-05 12:43:13 +0300

    add cat canvas generator

--- src/pages/Meme.tsx
+++ src/pages/Meme.tsx
@@ -1,58 +1,33 @@
-import React from 'react';
+import SLoader from 'components/SLoader'
+import React from 'react'
 
 const Meme = () => {
-    const [imageUrl, setImageUrl] = React.useState<string>('');
-	const [memeImage,setMemeImage] = React.useState<HTMLImageElement |null>(null);
-	const [imageObj, setImageObj] = React.useState<File | null>(null);
-	const canvasRef = React.useRef<HTMLCanvasElement>(null);
-	React.useEffect(() => {
-        const image = new Image()
-		image.onload = () => {
-			image.src = imageUrl;
-            image.onload=()=>setMemeImage(image)
-		};
-		// image.setAttribute('style', 'object-fit:contain');
-	},[imageUrl,imageObj])
-	React.useEffect(() => {
-		if (memeImage && imageUrl) {
-			const ctx = canvasRef.current?.getContext('2d');
-			ctx!.fillStyle = 'black';
-			ctx?.fillRect(0, 0, 400, 256 + 80);
-			ctx?.drawImage(memeImage, (400 - 256) / 2, 500);
-		}
-	}, [canvasRef, memeImage,imageUrl]);
-	React.useEffect(() => {
-		if (imageObj) {
-			setImageUrl(URL.createObjectURL(imageObj));
-		}
-	}, [imageObj]);
-    
-	return (
-		<div className='p-6'>
-			{/* <label htmlFor='file' className='p-4'>
-				Select a file
-			</label> */}
-			<input
-				type='file'
-				// hidden
-				id='file'
-				onChange={(e) => {
-					e.preventDefault();
-					setImageObj(e.target.files![0]);
-				}}
-			/>
-			<canvas width={500} height={406 + 80} ref={canvasRef}></canvas>
-            {/* {<memeImage/>} */}
-			{/* {imageUrl && (
-				<img
-					ref={imageRef}
-					src={imageUrl}
-					alt=''
-					style={{ width: 400, height: 400, objectFit: 'contain' }}
-				/>
-			)} */}
-		</div>
-	);
-};
+	const canvasRef = React.useRef<HTMLCanvasElement>(null)
+	const [catImage, setCatImage] = React.useState<any>(null)
 
-export default Meme;
+	React.useEffect(() => {
+		const image = new Image()
+		image.src = 'https://thiscatdoesnotexist.com'
+		image.onload = () => setCatImage(image)
+	}, [])
+
+	React.useEffect(() => {
+		if (catImage && canvasRef) {
+			const ctx = canvasRef.current!.getContext('2d')!
+			ctx.fillStyle = 'black'
+			ctx.fillRect(0, 0, 400, 256 + 80)
+			ctx.drawImage(catImage, 0, 40, 400, 256)
+		}
+
+	}, [catImage, canvasRef])
+
+	return (!(catImage) ? (<div className='w-full flex items-center justify-center py-6'>
+		<SLoader />
+	</div>) :
+		(<div className='w-full flex justify-center py-8'>
+			<canvas ref={canvasRef} width={400} height={256 + 80}></canvas>
+		</div>)
+	)
+}
+
+export default Meme
