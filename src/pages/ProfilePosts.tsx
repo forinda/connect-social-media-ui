6baef1dfd5dfc694a0982d0234728216e37fe9db
Felix Orinda <forinda82@gmail.com>
2022-07-05 13:03:01 +0300

    Update profile timeline

--- src/pages/ProfilePosts.tsx
+++ src/pages/ProfilePosts.tsx
@@ -2,32 +2,32 @@ import React from 'react'
 import { useAppSelector } from 'state/hooks'
 
 const ProfilePosts = () => {
-  const {personalPosts:{feeds},root:{user}}=useAppSelector(state=>state)
- 
+  const { personalPosts: { feeds }, root: { user } } = useAppSelector(state => state)
+
   return (
     <div className='flex flex-col gap-4 items-center'>
       {feeds.map((blog) => (
-      <div className=' w-full md:w-[40rem] shadow-[0_0_.5rem_.3rem_#cecece] rounded-md overflow-hidden' key={blog._id}>
-        {/* User */}
-        <div className='flex gap-4 items-center px-4 py-1 border-b'>
-          <div>
-            <img src="/girl4.webp" alt="" className='h-16 w-16 rounded-full' />
+        <div className=' w-full md:w-[40rem] shadow-[0_0_.5rem_.3rem_#cecece] rounded-md overflow-hidden' key={blog._id}>
+          {/* User */}
+          <div className='flex gap-4 items-center px-4 py-1 border-b'>
+            <div>
+              <img src={blog.author?.avatar.url ? blog.author?.avatar.url : '/user.svg'} alt="" className='h-16 w-16 rounded-full' />
+            </div>
+            <div>
+              <h2>{user.username}</h2>
+            </div>
           </div>
-          <div>
-            <h2>{blog.author?.username}</h2>
+          {/* Image iff */}
+          <div className='w-full h-[30rem]'>
+            <img src={blog.media.url?blog.media.url:'/post.webp'} alt="" className=' h-full object-cover' />
           </div>
-        </div>
-        {/* Image iff */}
-        <div className='w-full h-[30rem]'>
-          <img src={user.avatar.url} alt="" className=' h-full object-cover' />
-        </div>
-        {/* post body iff */}
-        <div>
-          <p className='p-4'>{blog.body}</p>
-        </div>
-        {/* Quick actions */}
-        <div></div>
-      </div>))}
+          {/* post body iff */}
+          <div>
+            <p className='p-4'>{blog.body}</p>
+          </div>
+          {/* Quick actions */}
+          <div></div>
+        </div>))}
     </div>
   )
 }
